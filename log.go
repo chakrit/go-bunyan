b8a009c5d74a4ac7d9e180803ed7244c868be923
Chakrit Wichian <service@chakrit.net>
2014-01-08 19:10:40 +0700

    Basic shared Log interface.

---
+++ log.go
@@ -0,0 +1,24 @@
+package bunyan
+
+// REF: https://github.com/trentm/node-bunyan#levels
+const(
+	EVERYTHING = 10 * iota
+	TRACE
+	DEBUG
+	INFO
+	WARN
+	ERROR
+	FATAL
+)
+
+type Log interface{
+	Add(key string, value interface{}) Log // builds new record
+	Child() Log // child logger with what's recorded so far
+
+	Tracef(msg string, args...interface{})
+	Debugf(msg string, args...interface{})
+	Infof(msg string, args...interface{})
+	Warnf(msg string, args...interface{})
+	Errorf(msg string, args...interface{})
+	Fatal(msg string, args...interface{})
+}
