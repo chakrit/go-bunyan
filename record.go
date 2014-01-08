f0b7af7d4f87fae0644c9846721a5aea4ee062f3
Chakrit Wichian <service@chakrit.net>
2014-01-08 19:17:38 +0700

    Implement MergeRecord

--- record.go
+++ record.go
@@ -14,3 +14,14 @@ func NewSimpleRecord(key string, value interface{}) Record {
 	record[key] = value
 	return record
 }
+
+func MergeRecord(records...Record) Record {
+	result := NewRecord()
+	for _, record := range records {
+		for k, v := range record {
+			result[k] = v
+		}
+	}
+
+	return result
+}
--- record_test.go
+++ record_test.go
@@ -16,3 +16,16 @@ func TestNewSimpleRecord(t *testing.T) {
 	a.NotNil(t, result, "cannot create simple record.")
 	a.Equal(t, len(result), 1, "result have incorrect length.")
 }
+
+func TestMergeRecord(t *testing.T) {
+	one := NewSimpleRecord("hello", "world")
+	one["argus"] = "was not here"
+	two := NewSimpleRecord("argus", "magnimus") // should override
+
+	result := MergeRecord(one, two)
+	a.NotNil(t, result, "merged record should not be nil.")
+	a.NotNil(t, result["hello"], "keys from first record not merged.")
+	a.Equal(t, result["hello"], "world", "keys from first record has wrong value.")
+	a.NotNil(t, result["argus"], "keys from second record not merged.")
+	a.Equal(t, result["argus"], "magnimus", "keys from second record has wrong value.")
+}
