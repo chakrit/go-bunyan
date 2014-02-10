336e4929d302a39707f86c0402a9dc0073030f19
Chakrit Wichian <service@chakrit.net>
2014-02-10 22:30:31 +0700

    Use `l` instead of `b` as logger's this.

--- logger.go
+++ logger.go
@@ -15,68 +15,51 @@ func NewLogger(target Sink) *Logger {
 
 // Write() writes the given record to the attached sink. If the receiver is a child
 // logger, then additional information from parent loggers will also be included.
-func (b *Logger) Write(record Record) error {
-	record.TemplateMerge(b.record)
-	return b.sink.Write(record)
+func (l *Logger) Write(record Record) error {
+	record.TemplateMerge(l.record)
+	return l.sink.Write(record)
 }
 
 // Include() returns a new logger instance that automatically records information from the
 // given Info instance to all its records.
-func (b *Logger) Include(info Info) Log {
-	return NewLogger(InfoSink(b, info))
+func (l *Logger) Include(info Info) Log {
+	return NewLogger(InfoSink(l, info))
 }
 
 // Record() adds the given key and value to the logger and returns itself.
 // TODO: Wrong doc
-func (b *Logger) Record(key string, value interface{}) Log {
+func (l *Logger) Record(key string, value interface{}) Log {
 	// TODO: Optimize. Don't New too damn much. Probably can get rid of TemplateMerge
 	// altogether.
-	builder := NewLogger(b)
+	builder := NewLogger(l)
 	builder.record[key] = value
 	return builder
 }
 
 // Recordf() provides formatting convenience that simply calls Record() with the formatted
 // value.
-func (b *Logger) Recordf(key, value string, args ...interface{}) Log {
+func (l *Logger) Recordf(key, value string, args ...interface{}) Log {
 	// TODO: Needs Child() calls
-	return b.Record(key, fmt.Sprintf(value, args...))
+	return l.Record(key, fmt.Sprintf(value, args...))
 }
 
 // Child() creates a child logger from the receiver logger. Any records written to the
 // child ogger will also contains information recorded in the parent logger.
-func (b *Logger) Child() Log {
-	return NewLogger(b)
+func (l *Logger) Child() Log {
+	return NewLogger(l)
 }
 
-func (b *Logger) Tracef(msg string, args ...interface{}) {
-	b.writef(TRACE, msg, args...)
-}
+func (l *Logger) Tracef(msg string, args ...interface{}) { l.send(TRACE, msg, args...) }
+func (l *Logger) Debugf(msg string, args ...interface{}) { l.send(DEBUG, msg, args...) }
+func (l *Logger) Infof(msg string, args ...interface{}) { l.send(INFO, msg, args...) }
+func (l *Logger) Warnf(msg string, args ...interface{}) { l.send(WARN, msg, args...) }
+func (l *Logger) Errorf(msg string, args ...interface{}) { l.send(ERROR, msg, args...) }
+func (l *Logger) Fatalf(msg string, args ...interface{}) { l.send(FATAL, msg, args...) }
 
-func (b *Logger) Debugf(msg string, args ...interface{}) {
-	b.writef(DEBUG, msg, args...)
-}
-
-func (b *Logger) Infof(msg string, args ...interface{}) {
-	b.writef(INFO, msg, args...)
-}
-
-func (b *Logger) Warnf(msg string, args ...interface{}) {
-	b.writef(WARN, msg, args...)
-}
-
-func (b *Logger) Errorf(msg string, args ...interface{}) {
-	b.writef(ERROR, msg, args...)
-}
-
-func (b *Logger) Fatalf(msg string, args ...interface{}) {
-	b.writef(FATAL, msg, args...)
-}
-
-func (b *Logger) writef(level Level, msg string, args ...interface{}) {
+func (l *Logger) send(level Level, msg string, args ...interface{}) {
 	record := NewRecord()
 	record.SetMessagef(level, msg, args...)
-	e := b.Write(record)
+	e := l.Write(record)
 
 	// TODO: Do not panic. Recover gracefully, maybe write something to stderr.
 	if e != nil {
